5641cc93ef741b70a763cc748c204684e0e26e27
Filip Barl <filip.barl@gmail.com>
2020-02-28 21:18:11 +0100

    Reuse TimeAxis component for TimeSlider

--- web/src/components/graph/areagraph.js
+++ web/src/components/graph/areagraph.js
@@ -145,10 +145,11 @@ const AreaGraph = React.memo(({
   if (isEmpty(layers)) return null;
 
   return (
-    <svg height="10em" ref={ref}>
+    <svg height="10em" style={{ overflow: 'visible' }} ref={ref}>
       <TimeAxis
         scale={timeScale}
-        height={container.height}
+        transform={`translate(-1 ${container.height - 1})`}
+        className={'x axis'}
       />
       <ValueAxis
         scale={valueScale}
